03129e5290ddf9094b402e4dff130fcf2f114d66
Prabhatt01 <prabhat2313119@akgec.ac.in>
2026-04-07 22:56:37 +0530

    Backend Connected

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,9 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        // target: 'http://localhost:5000', 
+        // target: 'https://quizapp-tjsd.onrender.com' ,
+        target : 'https://quiz-app-m9j8.vercel.app/',
         changeOrigin: true
       }
     }
